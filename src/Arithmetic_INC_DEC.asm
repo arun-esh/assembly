66a9853aa56e3bbc0064740ea4d31e7abe5539b2
nysdas <nysdas@icloud.com>
2021-10-29 13:46:26 -0400

    arithmetic INC and DEC

---
+++ src/Arithmetic_INC_DEC.asm
@@ -0,0 +1,41 @@
+TITLE
+
+; Name: 
+; Date: 
+; ID: 
+; Description: 
+
+INCLUDE Irvine32.inc
+INCLUDELIB Irvine32.lib
+
+; these two files are only necessary if you're not using Visual Studio
+INCLUDELIB kernel32.lib
+INCLUDELIB user32.lib
+
+.data
+
+	; data declarations go here
+	var1 BYTE 11
+	var2 BYTE 14
+
+.code
+main PROC
+	
+	; code goes here
+
+	DEC var1				; decreasing the value of var1
+	MOV eax, 0				; clearing EAX for clear visibility
+	MOV al, var1			; decreased value of var1 moved to al
+
+	INC var2
+	MOV ebx, 0
+	MOV bl, var2
+
+	
+
+	call DumpRegs ; displays registers in console
+
+	exit
+
+main ENDP
+END main
---
+++ src/example-imul.asm
@@ -0,0 +1,38 @@
+TITLE
+
+; Name: 
+; Date: 
+; ID: 
+; Description: 
+
+INCLUDE Irvine32.inc
+INCLUDELIB Irvine32.lib
+
+; these two files are only necessary if you're not using Visual Studio
+INCLUDELIB kernel32.lib
+INCLUDELIB user32.lib
+
+.data
+
+	; data declarations go here
+	var1 DWORD 10
+	PRODUCT DWORD ?
+
+
+.code
+main PROC
+	
+	; code goes here
+
+	MOV EAX, 5
+	MOV EBX, var1
+	iMUL EBX						; imul instruction multiply the content of EAX with the instruction operand 
+	MOV PRODUCT, EAX
+	
+
+	call DumpRegs ; displays registers in console
+
+	exit
+
+main ENDP
+END main
